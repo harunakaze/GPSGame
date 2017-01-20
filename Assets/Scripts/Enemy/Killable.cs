7a2f0e8222337b68d8483b07793013f141e8c5ca
KAZE\harunakaze <haruna.kaze@gmail.com>
2017-01-21 05:54:51 +0700

    - GitIngnore fix

---
+++ Assets/Scripts/Enemy/Killable.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Killable : MonoBehaviour {
+    public int hitPoints = 1;
+
+    void OnCollisionEnter2D(Collision2D other) {
+        if(other.gameObject.CompareTag("Projectile")) {
+
+        }
+    }
+}
