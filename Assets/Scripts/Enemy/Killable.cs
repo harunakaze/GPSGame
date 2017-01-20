8c2bd35bfaf6833f69fe10667f7d926faf39737f
KAZE\harunakaze <haruna.kaze@gmail.com>
2017-01-21 05:59:20 +0700

    + Killable enemy

--- Assets/Scripts/Enemy/Killable.cs
+++ Assets/Scripts/Enemy/Killable.cs
@@ -5,8 +5,17 @@ public class Killable : MonoBehaviour {
     public int hitPoints = 1;
 
     void OnCollisionEnter2D(Collision2D other) {
-        if(other.gameObject.CompareTag("Projectile")) {
+        Debug.Log("SS");
+        if(other.gameObject.CompareTag("Projectiles")) {
+            hitPoints--;
 
+            if(hitPoints <= 0) {
+                Die();
+            }
         }
+    }
+
+    void Die() {
+        Destroy(gameObject);
     }
 }
