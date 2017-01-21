4d5e5574a4d6ed77edb64314b54d1d9e16fad432
BintangSugiharto <bintangsugihartoputrautomo@gmail.com>
2017-01-21 18:45:26 +0700

    pwerup

---
+++ Assets/Scripts/Environments/Powerup.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Powerup : MonoBehaviour {
+    Transform tr;
+	// Use this for initialization
+	void Start () {
+        tr = transform;
+        InvokeRepeating("floating", 1, 2);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void floating() {
+        tr.Translate(new Vector3(Random.Range(-2, 2), Random.Range(-2, 2),tr.position.z)*Time.deltaTime*4);
+    }
+}
