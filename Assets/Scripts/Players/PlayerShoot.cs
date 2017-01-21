a324a2eb4e0f9e7633db20b88ba64d5beb3c4b03
KAZE\harunakaze <haruna.kaze@gmail.com>
2017-01-21 10:53:49 +0700

    Revert "Merge remote-tracking branch 'origin/master'"
    This reverts commit 4aaeb5dc6b5eb2f4bb4fd2d68177521996f8c1aa, reversing
    changes made to 835071d95d5040a37f99c4d028717f0f34e34c3b.

--- Assets/Scripts/Players/PlayerShoot.cs
+++ Assets/Scripts/Players/PlayerShoot.cs
@@ -6,33 +6,27 @@ public class PlayerShoot : MonoBehaviour {
 	public GameObject sw;
 	public GameObject ne;
 	public GameObject se;
-
-    public Transform nwPos;
-    public Transform swPos;
-    public Transform nePos;
-    public Transform sePos;
-
-    // Use this for initialization
-    void Start () {
+	// Use this for initialization
+	void Start () {
 	
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		if (Input.GetKeyDown(KeyCode.Q)) {
-		Instantiate (nw, nwPos.position, this.transform.rotation);
+		Instantiate (nw, this.transform.position, this.transform.rotation);
 			
 				}
 		if (Input.GetKeyDown(KeyCode.E)) {
-			Instantiate (ne, nePos.position, this.transform.rotation);
+			Instantiate (ne, this.transform.position, this.transform.rotation);
 			
 		}
 		if (Input.GetKeyDown(KeyCode.C)) {
-			Instantiate (se, sePos.position, this.transform.rotation);
+			Instantiate (se, this.transform.position, this.transform.rotation);
 			
 		}
 		if (Input.GetKeyDown(KeyCode.Z)) {
-			Instantiate (sw, swPos.position, this.transform.rotation);
+			Instantiate (sw, this.transform.position, this.transform.rotation);
 			
 		}
 		//		this.transform.position =new Vector3(this.transform.position.x,this.transform.position.y,this.transform.position.z);
