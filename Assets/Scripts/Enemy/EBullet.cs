835071d95d5040a37f99c4d028717f0f34e34c3b
BintangSugiharto <bintangsugihartoputrautomo@gmail.com>
2017-01-21 10:28:49 +0700

    Naganya nembak

---
+++ Assets/Scripts/Enemy/EBullet.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EBullet : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        this.gameObject.GetComponent<Rigidbody2D>().AddForce(transform.up * 12.0f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
--- Assets/Scripts/Naga/ShootingState.cs
+++ Assets/Scripts/Naga/ShootingState.cs
@@ -4,14 +4,34 @@ using System.Collections;
 public class ShootingState : PredatorBaseState {
     public float minBeforeMoving = 15.0f;
 
+    public GameObject blt;
+
     private Transform player;
 
+    float d=0;
+    bool s = true;
+
+    public Transform shot; 
     void Start() {
         player = GameObject.FindGameObjectWithTag("Player").transform;
     }
 
     public override void SUpdate() {
-        Shoot();
+        transform.up = player.position - transform.position;
+        d = d + Time.deltaTime;
+        Debug.Log(d);
+        if (d%2>=1&&s==true)
+        {
+            Shoot();
+            s = false;
+        }
+        else if (d%2<1&&s!=true)
+        {
+            s = true;
+            
+        }
+
+        
 
         if (Vector2.Distance(player.position, transform.position) >= minBeforeMoving) {
            
@@ -20,6 +40,9 @@ public class ShootingState : PredatorBaseState {
     }
 
     void Shoot() {
+        //Vector3 shootpos = new Vector3(this.transform.position.x - (0.1f * Mathf.Sin(this.transform.rotation.z)), this.transform.position.y + (0.1f * Mathf.Cos(this.transform.rotation.z)), this.transform.position.z);
+
+        Instantiate(blt, shot.position,this.transform.rotation);
 
     }
 }
