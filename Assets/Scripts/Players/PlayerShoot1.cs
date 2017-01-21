5f345892d700715e8db02e9259b1c3cfff55a2f2
BintangSugiharto <bintangsugihartoputrautomo@gmail.com>
2017-01-21 21:43:22 +0700

    New Reload Mechanic
    masukin playershoot1

---
+++ Assets/Scripts/Players/PlayerShoot1.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerShoot1 : MonoBehaviour {
+	public GameObject nw;
+	public GameObject sw;
+	public GameObject ne;
+	public GameObject se;
+
+    public Transform nwPos;
+    public Transform swPos;
+    public Transform nePos;
+    public Transform sePos;
+
+    public int magazine=15;
+    public int reload = 0;
+    // Use this for initialization
+    void Start () {
+	    
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.I)&&magazine>0) {
+		Instantiate (nw, nwPos.position, nwPos.rotation);
+        magazine--;
+				}
+        else if (Input.GetKeyDown(KeyCode.O) && magazine > 0)
+        {
+			Instantiate (ne, nePos.position, nePos.rotation);
+            magazine--;
+			
+		}
+        else if (Input.GetKeyDown(KeyCode.L) && magazine > 0)
+        {
+			Instantiate (se, sePos.position, sePos.rotation);
+            magazine--;
+			
+		}
+        else if (Input.GetKeyDown(KeyCode.K) && magazine > 0)
+        {
+			Instantiate (sw, swPos.position, swPos.rotation);
+            magazine--;
+			
+		}
+        if (magazine<=0)
+        {
+            if (Input.GetKeyDown(KeyCode.K) || Input.GetKeyDown(KeyCode.L) || Input.GetKeyDown(KeyCode.O) || Input.GetKeyDown(KeyCode.I))
+                reload++;
+            if (reload>=15)
+            {
+                reload = 0;
+                magazine = 15;
+            }
+        }
+		//		this.transform.position =new Vector3(this.transform.position.x,this.transform.position.y,this.transform.position.z);
+
+	}
+    
+}
