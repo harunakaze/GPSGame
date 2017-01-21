fbba99e9ecf916c78ab3e1f9a1192e0c17c76e5d
KAZE\harunakaze <haruna.kaze@gmail.com>
2017-01-21 16:00:18 +0700

    - Destroy neptune bullet

--- Assets/Scripts/OutOfBound.cs
+++ Assets/Scripts/OutOfBound.cs
@@ -4,19 +4,17 @@ using UnityEngine;
 
 public class OutOfBound : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
     void OnColliderEnter2D(Collider2D other)
     {
         if (other.gameObject.tag != "Player")
         {
+            Destroy(other.gameObject);
+
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other) {
+        if (other.gameObject.tag != "Player") {
             Destroy(other.gameObject);
 
         }
