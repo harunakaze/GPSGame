eb602ab13d55787cf52858a05f13ed86885fe66b
KAZE\harunakaze <haruna.kaze@gmail.com>
2017-01-22 09:34:12 +0700

    + Kidul sweet spot

---
+++ Assets/Scripts/Utility/GameManager.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+	void Update () {
+		if(Input.GetKeyDown(KeyCode.R)) {
+            Application.LoadLevel(Application.loadedLevel);
+        }
+	}
+}
