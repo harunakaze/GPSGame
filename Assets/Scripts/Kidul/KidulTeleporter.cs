f0d5542387b93aacf2b1377bc698e0b836dc81b8
KAZE\harunakaze <haruna.kaze@gmail.com>
2017-01-22 10:50:00 +0700

    + New Sprites

---
+++ Assets/Scripts/Kidul/KidulTeleporter.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KidulTeleporter : MonoBehaviour {
+
+    public Killable killable;
+
+    public Transform kidulPlaces2;
+    public Transform kidulPlaces3;
+
+    private int currentPos = 1;
+
+    private void Update() {
+        if(killable.hitPoints <= 32 && currentPos == 1) {
+            killable.hitPoints = 32;
+            transform.position = kidulPlaces2.position;
+            currentPos++;
+        } else if(killable.hitPoints <= 16 && currentPos == 2) {
+            killable.hitPoints = 16;
+            transform.position = kidulPlaces3.position;
+            currentPos++;
+        }
+    }
+
+}
--- Assets/Scripts/Utility/GameManager.cs
+++ Assets/Scripts/Utility/GameManager.cs
@@ -4,9 +4,15 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour {
 
+    public PlayerController kp;
+
 	void Update () {
 		if(Input.GetKeyDown(KeyCode.R)) {
             Application.LoadLevel(Application.loadedLevel);
         }
-	}
+
+        if(Input.GetKeyDown(KeyCode.Q)) {
+            kp.hitPoints = 20;
+        }
+    }
 }
